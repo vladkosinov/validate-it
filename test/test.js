15db6273b2d42e21f5a81ca2b2ba79602bdd7c3b
Vlad Kosinov <vladkosinov@gmail.com>
2014-05-07 04:31:51 +0800

    light tests

---
+++ test/test.js
@@ -0,0 +1,30 @@
+var validateIt = require('./../index.js');
+var should = require('should');
+
+var rules = [
+  {
+    name: 'username',
+    required: true,
+    len: [3, 20],
+    msg: {
+      required: 'Ohooho user name required'
+    }
+  },
+  {
+    name: 'password',
+    required: true,
+    len: [3, 20]
+  }
+];
+
+var validData = [
+  {username: 'vladko', password: 'qqq'}
+];
+
+describe('validate-it tests', function() {
+  it("should not find errors", function() {
+    validData.forEach(function (key) {
+      (validateIt(key, rules)).should.be.empty;
+    });
+  });
+});
